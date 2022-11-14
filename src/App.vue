a831e40efb4034ddc488b2a75bb4696ccb89f606
IRSAGE <sibo.egide98@gmail.com>
2022-11-14 14:22:51 +0200

    Adding Project Dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <h1>HelloWorld Welcome To Weather App</h1>
-  </div>
+  <v-app> </v-app>
 </template>
 
 <script>
@@ -9,19 +7,8 @@ export default {
   name: "App",
   data() {
     return {
-      api_key: "0a779abe3a448790cdbd45cfee943301",
+      api_keys: "0a779abe3a448790cdbd45cfee943301",
     };
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
