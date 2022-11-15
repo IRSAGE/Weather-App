f5bd9595eba295f534cb6e43048ed2a3f5f3b406
IRSAGE <sibo.egide98@gmail.com>
2022-11-15 12:24:41 +0200

    Separating weather component

--- src/App.vue
+++ src/App.vue
@@ -29,20 +29,9 @@
       "
     >
       <main>
-        <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
-          <div class="location-box">
-            <div class="location">
-              {{ weather.name }}, {{ weather.sys.country }}
-            </div>
-            <div class="date">{{ dateBuilder() }}</div>
-          </div>
+        <WeatherComponent :weather="weather" />
 
-          <div class="weather-box">
-            <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
-            <div class="weather">{{ weather.weather[0].main }}</div>
-          </div>
-        </div>
-        <div v-if="this.notFound" class="notfound">
+        <div v-if="this.notFound == true" class="notfound">
           <h2 class="notfound-text">No Weather Found!</h2>
         </div>
       </main>
@@ -52,9 +41,12 @@
 
 <script>
 import axios from "axios";
+import WeatherComponent from "./components/WeatherComponent.vue";
 export default {
   name: "App",
-
+  components: {
+    WeatherComponent,
+  },
   data() {
     return {
       api_key: "0a779abe3a448790cdbd45cfee943301",
@@ -81,37 +73,6 @@ export default {
           });
       }
     },
-    dateBuilder() {
-      let d = new Date();
-      let months = [
-        "January",
-        "February",
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December",
-      ];
-      let days = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-      ];
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
-      return `${day} ${date} ${month} ${year}`;
-    },
   },
 };
 </script>
@@ -135,45 +96,7 @@ main {
     rgba(0, 0, 0, 0.75)
   );
 }
-.location-box .location {
-  color: #fff;
-  font-size: 32px;
-  font-weight: 500;
-  text-align: center;
-  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
-}
-.location-box .date {
-  color: #fff;
-  font-size: 20px;
-  font-weight: 300;
-  font-style: italic;
-  text-align: center;
-}
-.weather-box {
-  text-align: center;
-}
-.weather-box .temp {
-  display: inline-block;
-  padding: 10px 25px;
-  color: #fff;
-  font-size: 102px;
-  font-weight: 900;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  background-color: rgba(255, 255, 255, 0.25);
-  border-radius: 16px;
-  margin: 30px 0px;
-  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-.weather-box .weather {
-  color: #fff;
-  font-size: 48px;
-  font-weight: 700;
-  font-style: italic;
-  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-}
-.weather-wrap {
-  align-self: center;
-}
+
 .notfound {
   display: flex;
   justify-content: center;
---
+++ src/components/WeatherComponent.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+    <div class="location-box">
+      <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+      <div class="date">{{ dateBuilder() }}</div>
+    </div>
+
+    <div class="weather-box">
+      <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+      <div class="weather">{{ weather.weather[0].main }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WeatherComponent",
+  props: ["weather"],
+  methods: {
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-wrap {
+  align-self: center;
+}
+</style>
